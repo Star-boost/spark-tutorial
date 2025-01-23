99df68150242438ff5ce110b14f9c7c8f5d83032
Akindu Dasanayake <akindu@mac.lan>
2025-01-23 18:17:57 -0500

    section 16 on getting started with sparksql

--- Main.java
+++ Main.java
@@ -1,64 +1,26 @@
 package com.virtualpairprogrammers;
 
-import java.util.List;
-import java.util.Arrays;
-import java.util.Scanner;
-
-import scala.Tuple2;
-
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
 
 public class Main {
     public static void main(String[] args) {
         Logger.getLogger("org").setLevel(Level.WARN);
 
-        SparkConf conf = new SparkConf().setAppName("startingSpark").setMaster("local[*]");
-        JavaSparkContext sc = new JavaSparkContext(conf);
+        SparkSession spark = SparkSession.builder().appName("testingSql").master("local[*]").getOrCreate();
 
-        // JavaRDD<String> initialRdd =
-        // sc.textFile("s3://akindu-spark-demos/input.txt");
-        JavaRDD<String> initialRdd = sc.textFile("src/main/resources/subtitles/input.txt");
+        Dataset<Row> dataset = spark.read().option("header", true).csv("src/main/resources/exams/students.csv");
+        dataset.show();
 
-        JavaRDD<String> lettersOnlyRdd = initialRdd
-                .map(sentence -> sentence.replaceAll("[^a-zA-Z\\s]", "").toLowerCase());
+        // count is probably a map reduce done on an rdd under the hood, but we don't
+        // have to think about it
+        long numberOfRows = dataset.count();
+        System.out.println("There are " + numberOfRows + " records");
 
-        JavaRDD<String> removedBlankLines = lettersOnlyRdd.filter(setence -> setence.trim().length() > 0);
+        spark.close();
 
-        JavaRDD<String> justWords = removedBlankLines
-                .flatMap(sentence -> Arrays.asList(sentence.split(" ")).iterator());
-
-        JavaRDD<String> blankWordsRemoved = justWords.filter(word -> word.trim().length() > 0);
-
-        JavaRDD<String> justInterestingWords = blankWordsRemoved.filter(word -> Util.isNotBoring(word));
-
-        JavaPairRDD<String, Long> pairRdd = justInterestingWords.mapToPair(word -> new Tuple2<>(word, 1L));
-
-        JavaPairRDD<String, Long> totals = pairRdd.reduceByKey((value1, value2) -> value1 + value2);
-
-        JavaPairRDD<Long, String> switched = totals.mapToPair(tuple -> new Tuple2<Long, String>(tuple._2, tuple._1));
-
-        JavaPairRDD<Long, String> sorted = switched.sortByKey(false);
-
-        // this is the only place where spark HAS to do something. Everything else was
-        // just building the execution plan
-        // Operations like this are called actions
-        // Other operations (like the ones above) are transformations
-        List<Tuple2<Long, String>> results = sorted.take(10);
-
-        results.forEach(System.out::println);
-
-        // Adding a scanner to wait for text input and allow access to the web ui on
-        // spark (localhost:4040)
-        Scanner scanner = new Scanner(System.in);
-        scanner.nextLine();
-
-        // scanner.close();
-
-        sc.close();
     }
 }
