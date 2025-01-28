129f8ca990e763775a559bcd5bec2bf0b4ad5cec
Akindu Dasanayake <akindu@mac.lan>
2025-01-28 15:48:32 -0500

    section 19 on in memory data

--- Main.java
+++ Main.java
@@ -1,12 +1,21 @@
 package com.virtualpairprogrammers;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.api.java.function.FilterFunction;
 import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+
 import static org.apache.spark.sql.functions.*;
 
 public class Main {
@@ -14,36 +23,30 @@ public class Main {
         Logger.getLogger("org").setLevel(Level.WARN);
 
         SparkSession spark = SparkSession.builder().appName("testingSql").master("local[*]").getOrCreate();
+        // Making our own dataframe:
 
-        Dataset<Row> dataset = spark.read().option("header", true).csv("src/main/resources/exams/students.csv");
+        // step 1: initialize arraylist
+        List<Row> inMemory = new ArrayList<Row>();
 
-        // Dataset<Row> modernArtResults = dataset.filter("select * from students where
-        // subject = 'Modern Art' AND year >= 2007");
+        // step 2: add data to list using row factory
+        inMemory.add(RowFactory.create("WARN", "2016-12-31 04:19:32"));
+        inMemory.add(RowFactory.create("FATAL", "2016-12-31 03:22:34"));
+        inMemory.add(RowFactory.create("WARN", "2016-12-31 03:21:21"));
+        inMemory.add(RowFactory.create("INFO", "2015-4-21 14:32:21"));
+        inMemory.add(RowFactory.create("FATAL", "2015-4-21 19:23:20"));
 
-        // we can do full sql syntax
+        // step 3: Initialize structfield
+        StructField[] fields = new StructField[] {
+                new StructField("level", DataTypes.StringType, false, Metadata.empty()),
+                new StructField("datetime", DataTypes.StringType, false, Metadata.empty())
+        };
+        // step 4: Initialize schema using the fields
+        StructType schema = new StructType(fields);
+        // Data frame is a dataset of rows
+        // step 5: create data frame using in memory list
+        Dataset<Row> dataset = spark.createDataFrame(inMemory, schema);
 
-        // we can build an actual table in memory, a "view":
-        dataset.createOrReplaceTempView("my_students_table");
-
-        // using the original spark session we built:
-
-        // Some sample sql queries:
-
-        // Dataset<Row> results = spark.sql("select * from my_students_table where
-        // subject = 'French'");
-
-        // Dataset<Row> results = spark.sql("select score, year from my_students_table
-        // where subject = 'French'");
-
-        // Dataset<Row> results = spark.sql("select max(score) from my_students_table
-        // where subject = 'French'");
-
-        // Dataset<Row> results = spark.sql("select avg(score) from my_students_table
-        // where subject = 'French'");
-
-        Dataset<Row> results = spark.sql("select distinct(year) from my_students_table order by year desc");
-
-        results.show();
+        dataset.show();
         spark.close();
 
     }
